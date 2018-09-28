9744284303de1357589fd6d32285f1e3b17c9ef5
Toastation <toastations@gmail.com>
2018-09-28 19:13:04 +0200

    added json decoding

--- demo/test_server_request_python.py
+++ demo/test_server_request_python.py
@@ -1,6 +1,8 @@
-# Just the equivalent of the CURL command in Python 2
+# Just the equivalent of the CURL command from the doc in Python 2
+# Haven't tested it yet I can't run the server on Windows...
 
 import requests # http://docs.python-requests.org/en/master/
+import json
 
 message = "I want to order chinese from mainland china and pizza from domminos"
 entity_name = "restaurant"
@@ -12,6 +14,7 @@ url = "localhost"
 port = "80"
 complete_url = "http://{}:{}/v1/text/".format(url, port)
 
-output = requests.post(complete_url, data={"message":message, "entity_name":entity_name, "structured_value":structured_value, "fallback_value":fallback_value, "bot_message":bot_message})
+output_json = requests.post(complete_url, data={"message":message, "entity_name":entity_name, "structured_value":structured_value, "fallback_value":fallback_value, "bot_message":bot_message})
+output = json.loads(output_json)
 
 print output
