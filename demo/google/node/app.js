27dd67c9f0abd19878d6841936f5976ff7449ad2
Toastation <toastations@gmail.com>
2018-10-01 23:07:32 +0200

    Switched to english var names; google_api.js -> demo.js for clarity; universal url for socket.io package; formatting

--- demo/google/node/app.js
+++ demo/google/node/app.js
@@ -3,6 +3,7 @@ var app = require('express')(),
     io = require('socket.io').listen(server),
     ent = require('ent'), // Permet de bloquer les caractères HTML (sécurité équivalente à htmlentities en PHP)
     fs = require('fs');
+
 // Imports the Google Cloud client library
 const language = require('@google-cloud/language');
 
@@ -11,49 +12,44 @@ const client = new language.LanguageServiceClient();
 
 // Chargement de la page index.html
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+    res.sendfile(__dirname + '/index.html');
 });
 
-io.sockets.on('connection', function (socket, pseudo) {
+io.sockets.on('connection', function (socket, nickname) {
     // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
-    socket.on('nouveau_client', function(pseudo) {
-        pseudo = ent.encode(pseudo);
-        socket.pseudo = pseudo;
-        socket.broadcast.emit('nouveau_client', pseudo);
+    socket.on('new_client', function(nickname) {
+        nickname = ent.encode(nickname);
+        socket.nickname = nickname;
+        socket.broadcast.emit('new_client', nickname);
     });
 
     // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
     socket.on('message', function (message) {
         message = ent.encode(message);
-        syntax_text('Test');
-        socket.emit('message',{pseudo: 'chatbot', message: 'J\'ai bien reçus ton message'});
-        //socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
+        syntax_text(message);
+        socket.emit('message', {pseudo: 'chatbot', message: 'J\'ai bien reçus ton message'});
+        //socket.broadcast.emit('message', {nickname: socket.nickname, message: message});
     });
 });
 
 server.listen(8080);
 
-
 function syntax_text(text){
- // Prepares a document, representing the provided text
- const document = {
-   content: text,
-   type: 'PLAIN_TEXT',
- };
+    // Prepares a document, representing the provided text
+    const document = {
+        content: text,
+        type: 'PLAIN_TEXT',
+    };
 
- // Detects syntax in the document
- client
-   .analyzeSyntax({document: document})
-   .then(results => {
-     const syntax = results[0];
-
-     console.log('Tokens:');
-     syntax.tokens.forEach(part => {
-       console.log(`${part.partOfSpeech.tag}: ${part.text.content}`);
-       console.log(`Morphology:`, part.partOfSpeech);
-     });
-   })
-   .catch(err => {
-     console.error('ERROR:', err);
-   });
+    // Detects syntax in the document
+    client.analyzeSyntax({document: document}).then(results => {
+        const syntax = results[0];
+        console.log('Tokens:');
+        syntax.tokens.forEach(part => {
+            console.log(`${part.partOfSpeech.tag}: ${part.text.content}`);
+            console.log(`Morphology:`, part.partOfSpeech);
+        });
+    }).catch(err => {
+        console.error('ERROR:', err);
+    });
 }
---
+++ demo/google/node/demo.js
@@ -0,0 +1,108 @@
+// Imports the Google Cloud client library
+const language = require('@google-cloud/language');
+
+// Creates a client
+const client = new language.LanguageServiceClient();
+
+function sentiment_text (text){
+    // Prepares a document, representing the provided text
+    const document = {
+        content: text,
+        type: 'PLAIN_TEXT',
+    };
+
+    // Detects the sentiment of the document
+    client.analyzeSentiment({document: document}).then(results => {
+        const sentiment = results[0].documentSentiment;
+        console.log(`Document sentiment:`);
+        console.log(`  Score: ${sentiment.score}`);
+        console.log(`  Magnitude: ${sentiment.magnitude}`);
+
+        const sentences = results[0].sentences;
+        sentences.forEach(sentence => {
+        console.log(`Sentence: ${sentence.text.content}`);
+        console.log(`Score: ${sentence.sentiment.score}`);
+        console.log(`Magnitude: ${sentence.sentiment.magnitude}`);
+        });
+    })
+ }
+
+function syntax_text(text){
+    // Prepares a document, representing the provided text
+    const document = {
+        content: text,
+        type: 'PLAIN_TEXT',
+    };
+
+    // Detects syntax in the document
+    client.analyzeSyntax({document: document}).then(results => {
+        const syntax = results[0];
+        console.log('Tokens:');
+        syntax.tokens.forEach(part => {
+            console.log(`${part.partOfSpeech.tag}: ${part.text.content}`);
+            console.log(`Morphology:`, part.partOfSpeech);
+        });
+    }).catch(err => {
+        console.error('ERROR:', err);
+    });
+}
+
+function entities_text(text) {
+    // Prepares a document, representing the provided text
+    const document = {
+        content: text,
+        type: 'PLAIN_TEXT',
+    };
+
+    // Detects entities in the document
+    client.analyzeEntities({document: document}).then(results => {
+        const entities = results[0].entities;
+        console.log('Entities:');
+        entities.forEach(entity => {
+            console.log(entity.name);
+            console.log(` - Type: ${entity.type}, Salience: ${entity.salience}`);
+            if (entity.metadata && entity.metadata.wikipedia_url) {
+                console.log(` - Wikipedia URL: ${entity.metadata.wikipedia_url}$`);
+            }
+        });
+    }).catch(err => {
+        console.error('ERROR:', err);
+    });
+}
+
+function entities_sentiment_text(text) {
+    const document = {
+      content: text,
+      type: 'PLAIN_TEXT',
+    };
+
+    // Detects sentiment of entities in the document
+    client.analyzeEntitySentiment({document: document}).then(results => {
+        const entities = results[0].entities;
+        console.log(`Entities and sentiments:`);
+        entities.forEach(entity => {
+            console.log(`  Name: ${entity.name}`);
+            console.log(`  Type: ${entity.type}`);
+            console.log(`  Score: ${entity.sentiment.score}`);
+            console.log(`  Magnitude: ${entity.sentiment.magnitude}`);
+      });
+    }).catch(err => {
+      console.error('ERROR:', err);
+    });
+}
+
+//Sentiment demo
+console.log('************* Entity detection demo');
+sentiment_text("Salut! Est-ce que tout va bien ?");
+
+//Syntax demo
+console.log('************* Syntax demo');
+syntax_text("Bonjour, ceci est un test.");
+
+//Entity detection demo
+console.log('************* Entity detection demo');
+entities_text("Bonjour, je veux aller a Paris");//doesn't support accents (?)
+
+//Entity sentiment detection demo
+console.log('************* Entity sentiment detection demo');
+entities_text("Bonjour, je voudrais aller a Paris s'il vous plait!");
