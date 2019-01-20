8ec389916d5b1ea4b0ea395ec9d9adf3092cc634
Toastation <toastations@gmail.com>
2019-01-20 19:19:22 +0100

    backend

--- demo/dialogflow/integration/app.js
+++ demo/dialogflow/integration/app.js
@@ -1,94 +1,31 @@
-/**
- * Quick TODO:
- *      - Handle promise (sessionClient.detectIntent) v
- *      - Use socket.io instead of the command line v
- */
-
-const dialogflow = require('dialogflow');
-const readline = require('readline');
-const structjson = require('structjson')
-const uuid = require('uuid'); // used to generate session id
-
 const express = require('express');
+const http = require('http');
+const sock = require('socket.io');
+const ent = require('ent'); 
+const fs = require('fs');
+const ApiInterface = require('./backend.js');
+
 const app = express();
-const server = require('http').createServer(app);
+const server = http.createServer();
+const io = sock.listen(server);
+const api = new ApiInterface();
 
-const io = require('socket.io').listen(server);
-const ent = require('ent'); // Permet de bloquer les caractères HTML (sécurité équivalente à htmlentities en PHP)
-const  fs = require('fs');
-// bot parameters
-const projectId = 'test-cdda6';
-const languageCode = 'fr-FR';
-const sessionId = uuid.v4();
-const sessionClient = new dialogflow.SessionsClient();
+const port = 3000;
 
-
-const sessionPath = sessionClient.sessionPath(projectId, sessionId);
-
-var result;
-var currentContexts; // the list of current contexts
-
-
-
-
-/**
- * Sends a request to the bot and log the response.
- * @param {string} The sentence of the user
- */
-async function parseResponse(answer) {
-    // create the request
-    const request = {
-        session: sessionPath,
-        queryInput: {
-            text: {
-                text: answer,
-                languageCode: languageCode,
-            },
-        },
-        queryParams: {
-            contexts: currentContexts,
-        },
-    };
-
-    // send the request
-    const responses = await sessionClient.detectIntent(request).catch(
-      (error) => {
-        console.log(error);
-      }
-    );
-    result = responses[0].queryResult;
-
-    // Update the current context with the output context
-    // There is a bug in gRPC that the returned google.protobuf.Struct
-    // value contains fields with value of null, which causes error
-    // when encoding it back. Converting to JSON and back to proto
-    // removes those values.
-    result.outputContexts.forEach(context => {
-        context.parameters = structjson.jsonToStructProto(
-            structjson.structProtoToJson(context.parameters)
-        );
-    });
-    currentContexts = result.outputContexts;
-    console.log("#contexts: ", currentContexts);
-
-    // log the bot response
-    var intentName = result.intent ? result.intent.displayName : "no intent matched";
-    console.log(result.fulfillmentText, '( intent:', intentName, ', confidence: ', result.intentDetectionConfidence ,')');
-}
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
 io.sockets.on('connection', function (socket) {
-    console.log('Un nouvel utilisateur s\'est connecter');
+    console.log('new user connected: ' + socket.client.id);
     socket.on('message', function (message) {
-        parseResponse(message).then(
-          ()=>{
-            socket.emit('message', {nickname: 'Chatbot', message: result.fulfillmentText, intention: result.intent.displayName, score: result.intentDetectionConfidence });
-          });
+        request = api.createRequest(message);
+        api.sendRequest(request).then((result) => {
+            socket.emit('message', {nickname: 'Chatbot', message: result.fulfillmentText, intention: result.intent.displayName, score: result.intentDectectionConfidence});
+        });
     });
 });
 
-server.listen(3000, function () {
+server.listen(port, function () {
   console.log('Example app listening on port 3000!')
 });
---
+++ demo/dialogflow/integration/backend.js
@@ -0,0 +1,71 @@
+const dialogflow = require('dialogflow');
+const structjson = require('structjson')
+const uuid = require('uuid');
+
+class ApiInterface {
+
+    constructor() {
+        this.init();
+    }
+
+    /**
+     * Initializes the session client
+     */
+    init() {
+        this.projectId = 'test-cdda6';
+        this.sessionId = uuid.v4();
+        this.sessionClient = new dialogflow.SessionsClient();
+        this.sessionPath = this.sessionClient.sessionPath(this.projectId, this.sessionId);
+        this.languageCode = 'fr-FR';
+        this.currentContexts = [];
+    }
+
+    /**
+     * https://cloud.google.com/dialogflow-enterprise/docs/reference/rpc/google.cloud.dialogflow.v2#google.cloud.dialogflow.v2.DetectIntentRequest
+     * Creates a DetectIntentRequest
+     * @param {string} message The message to send to the bot
+     * @returns The new request 
+     */
+    createRequest(message) {
+        const request = {
+            session: this.sessionPath,
+            queryInput: {
+                text: {
+                    text: message,
+                    languageCode: this.languageCode,
+                },
+            },
+            queryParams: {
+                contexts: this.currentContexts,
+            },
+        };
+        return request;
+    }
+
+    /**
+     * Sends a request (message) to bot and returns a Promise containing a DetectIntentResponse
+     * @param {DetectIntentRequest} request The formatted request to send (https://cloud.google.com/dialogflow-enterprise/docs/reference/rpc/google.cloud.dialogflow.v2#google.cloud.dialogflow.v2.DetectIntentRequest)
+     * @returns {DetectIntentResponse} The response of the bot
+     */
+    async sendRequest(request) {
+        const responses = await this.sessionClient.detectIntent(request).catch((error) => {
+            console.error(error);
+        });
+        let result = responses[0].queryResult;
+
+        // There is a bug in gRPC that the returned google.protobuf.Struct value contains fields with value of null, which causes error
+        // when encoding it back. Converting to JSON and back to proto removes those values.
+        result.outputContexts.forEach(context => {
+            context.parameters = structjson.jsonToStructProto(
+                structjson.structProtoToJson(context.parameters)
+            );
+        });
+        this.currentContexts = result.outputContexts;
+
+        return new Promise((resolve, reject) => {
+            resolve(result);
+        });
+    }
+}
+
+module.exports = ApiInterface;
