f416556bf6f3e9f82373fc894fdd87e5a21474e2
Meplos <alexxd64150@gmail.com>
2019-02-01 15:16:49 +0100

    separation des client pour api

--- demo/dialogflow/integration/app.js
+++ demo/dialogflow/integration/app.js
@@ -5,21 +5,27 @@ var ApiInterface = require('./backend.js');
 var app = require('express')();
 var server = http.createServer(app);
 var io = sock.listen(server);
-const api = new ApiInterface();
+
 
 const port = 3000;
+
+var client_arr = new Object();
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
 io.sockets.on('connection', function (socket) {
+  if(client_arr[socket.id] == undefined ){
+    client_arr[socket.id] = new ApiInterface();
+  }else{
+    //TODO: Redonner historique
+  }
     console.log('new user connected: ' + socket.client.id);
     socket.on('message', function (message) {
         request = api.createRequest(message);
-        api.sendRequest(request).then((result) => {
-            let quick_reponses = api.getQuickResponses(result); // api call example
-            console.log(typeof quick_reponses);
+        client_arr[socket.id].sendRequest(request).then((result) => {
+            let quick_reponses = client_arr[socket.id].getQuickResponses(result); // api call example
             socket.emit('message', {nickname: 'Chatbot', message: result.fulfillmentText, intention: result.intent.displayName, score: result.intentDetectionConfidence, quick_reponses: quick_reponses});
         });
     });
