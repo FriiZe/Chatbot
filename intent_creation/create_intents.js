aa0ecb9b7da9088d623ec17202b1a8289fc060e9
Toastation <toastations@gmail.com>
2019-03-09 16:17:41 +0100

    avancee script js

--- intent_creation/create_intents.js
+++ intent_creation/create_intents.js
@@ -3,9 +3,12 @@ const uuid = require('uuid');
 const dialogflow = require('dialogflow');
 
 const projectId = "test-cdda6";
-const sessionId = uuid.v4();
+const sessionIdContext = uuid.v4();
 const contextsClient = new dialogflow.ContextsClient();
-const contextSessionPath = contextsClient.sessionPath(projectId, sessionId);
+const intentsClient = new dialogflow.IntentsClient();
+const contextSessionPath = contextsClient.sessionPath(projectId, sessionIdContext);
+const agentPath = intentsClient.projectAgentPath(projectId);
+const lang = "fr";
 
 let file_format = /([a-zA-Z0-9\_\-\(\)\\\s])+.json/;
 let files = [];
@@ -29,42 +32,127 @@ if (process.argv.length <= 2) {
     }
 }
 
-// parse all files
-for (const file of files) {
-    console.log("Parsing file \'"+file+"\'...");
-    let fileData = fs.readFileSync(file);
-    let intents = JSON.parse(fileData);
+// list all current intents id and names
+let intentsList = [];
+initIntentsList().then(res => {parseFiles();}).catch(err => {console.error(err);});
 
-    // create contexts
-    for (const intent of intents) {
-        const input = intent.input;
-        const output = intent.output;
-        if (contextPaths[input] == undefined && input != "") 
-            createContextRequest(input);
-        if (contextPaths[output] == undefined && output != "") 
-            createContextRequest(output);
-    }
+function parseFiles() {
+    for (const file of files) {
+        console.log("Parsing file \'"+file+"\'...");
+        let fileData = fs.readFileSync(file);
+        let intents = JSON.parse(fileData);
     
-    // create intents
-    for (const intent of intents) {
-        console.log("  Creating intent \'"+intent.label+"\'");
-        let request = createIntentRequest(intent);
+        // create contexts
+        for (const intent of intents) {
+            const input = intent.input;
+            const output = intent.output;
+            if (contextPaths[input] == undefined && input != "") { 
+                createContextRequest(input).then(res => {
+                    console.log("  Context %s successfully added", input);
+                }).catch(err => {
+                    console.error(err);
+                });
+            }
+            if (contextPaths[output] == undefined && output != "") {
+                createContextRequest(output).then(res => {
+                    console.log("  Context %s successfully added", output);
+                }).catch(err => {
+                    console.error(err);
+                });
+            }
+        }
+        
+        // create intents
+        for (const intent of intents) {
+            console.log("  Creating intent \'"+intent.label+"\'");
+            if (intentAlreadyExist(intent.label)) {
+                console.log("  Intent %s already exists, updating it...", intent.label)
+                deleteIntent(intent).then(res => {
+                    // console.log("  Intent %s deleted", intent.label); 
+                    // createIntentRequest(intent).then(res => {
+                    //     console.log("  Intent %s successfully added", res[0])
+                    // }).catch((err) => {
+                    //     console.error(err.details);});
+                }).catch(err => {
+                        console.error(err);
+                });
+            } else {
+                createIntentRequest(intent).then(res => {
+                    console.log("  Intent %s successfully added", res.displayName);
+                }).catch((err) => {
+                    console.error(err.details);
+                });
+            }
+        }
     }
-    console.log("Finished parsing file \'"+file+"\'");
+}
+
+async function initIntentsList() {
+    const request = {
+        parent: agentPath,
+    };
+    const [res] = await intentsClient.listIntents(request);    
+    res.forEach(i => {
+        intentsList.push([i.name, i.displayName]);
+    });
+}
+
+function intentAlreadyExist(intentName) {
+    for (const intent of intentsList) {
+        if (intentName == intent[1])
+            return true; 
+    }
+    return false;
+}
+
+function getId(intentName) {
+    for (const intent of intentsList) {
+        if (intent[1] == intentName)
+            return intent[0];
+    }
+    return "";
 }
 
 /**
  * Takes an intent object from the json and wraps it in a request for the API
  */
-function createIntentRequest(intent) {
-
+async function createIntentRequest(intent) {
+    const trainingPhrases = [];
+    (intent.training_sentences).forEach(trainingSentencePart => {
+        const part = {
+            text: trainingSentencePart
+        };
+        const trainingPhrase = {
+            type: 'EXAMPLE',
+            parts: [part]
+        };
+        trainingPhrases.push(trainingPhrase);
+    });
+    const messageText = {
+        text: [intent.response]
+    };
+    const message = {
+        text: messageText
+    };
+    const intentRequest = {
+        displayName: intent.label,
+        trainingPhrases: trainingPhrases,
+        messages: [message]
+    };
+    const createIntentRequest = {
+        parent: agentPath,
+        intent: intentRequest,
+        languageCode: lang
+    };
+    const res = await intentsClient.createIntent(createIntentRequest);
+    return res;
 }
 
 /**
  * Takes a context object from the json and wraps it in a request for the API
  */
 async function createContextRequest(context) {
-    const contextPath = contextsClient.contextPath(projectId, sessionId, context);
+    const contextPath = contextsClient.contextPath(projectId, sessionIdContext, context);
     contextPaths[context] = contextPath;
     const request = {
         parent: contextSessionPath,
@@ -73,6 +161,12 @@ async function createContextRequest(context) {
           lifespanCount: 1
         }
     };
-    let res = await contextsClient.createContext(request);
+    const res = await contextsClient.createContext(request);
     return res;
+}
+
+async function deleteIntent(intent) {
+    const id = getId(intent.label);
+    const request = {name: id};
+    return await intentsClient.deleteIntent(request);
 }
