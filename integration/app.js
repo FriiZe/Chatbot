cd263cf556f0a7ac9d0e4fb9ee1a6f50651348ea
Meplos <alexxd64150@gmail.com>
2019-04-05 13:45:35 +0200

    comment app.js

--- integration/app.js
+++ integration/app.js
@@ -9,10 +9,14 @@ var io = sock.listen(server);
 const PORT = process.env.PORT || 5000;
 let clientList = new Object();
 
+/*
+* Send html page to client
+*/
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
+/*Initialise user event on user connexion */
 io.sockets.on('connection', function(socket) {
     if(clientList[socket.id] == undefined) {  // first connection
         clientList[socket.id] = new BotSession();
@@ -25,9 +29,9 @@ io.sockets.on('connection', function(socket) {
 
     // when receiving a user message, we wrap it in a "request", send it via the API and send back the response to the interface
     socket.on('message', function (message) {
-        let request = botSession.createRequest(message);
-        clientList[socket.id].messages.push({"isMe" : true, "message" : message});
-        botSession.sendRequest(request).then((result) => {
+        let request = botSession.createRequest(message); //Create user request 
+        clientList[socket.id].messages.push({"isMe" : true, "message" : message}); //add user message to history conversation
+        botSession.sendRequest(request).then((result) => { //send request to DialogFlow
             let quick_reponses = botSession.getQuickResponses(result); // api call example
             let payload = {
                 nickname: 'Chatbot',
@@ -35,10 +39,10 @@ io.sockets.on('connection', function(socket) {
                 intention: (result.intent != null ? result.intent.displayName : "no intent"),
                 score: result.intentDetectionConfidence,
                 quick_reponses: quick_reponses
-            };
-            clientList[socket.id].messages.push({"isMe" : false, "message" : payload.message});
+            }; //retrieve data  
+            clientList[socket.id].messages.push({"isMe" : false, "message" : payload.message}); //add bot message to history conversation
             console.log(clientList[socket.id].messages);
-            socket.emit('message', payload);
+            socket.emit('message', payload); //send response to user
         }).catch(function (error) {
             console.error(error);
         });
@@ -47,10 +51,11 @@ io.sockets.on('connection', function(socket) {
     // when the connection is closed
     socket.on('disconnect', function () {
         console.log("deleting instance");
-        delete clientList[socket.id];
+        delete clientList[socket.id]; //delete user to user list
     });
 });
 
+/*Lauch server */
 server.listen(PORT, function () {
     console.log('*** Listening on port '+PORT)
 });
